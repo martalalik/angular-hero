17ed00b0c7ddc8e093b9ea5cbaee0a8b6062d687
martalalik <105340174+martalalik@users.noreply.github.com>
2022-11-28 16:07:44 +0100

    Add a dashboard view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,17 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, ComponentOverviewComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    ComponentOverviewComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
